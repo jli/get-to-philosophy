1c123df54a4671a45dd3ce9c1589164c67a2eee8
John Li <jli@circularly.org>
2011-07-28 05:01:54 -0400

    first shot - mostly there...

---
+++ hops-to__local/project.clj
@@ -0,0 +1,6 @@
+(defproject hops-to "1.0.0-SNAPSHOT"
+  :description "Hops to a wiki page"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [enlive "1.0.0-SNAPSHOT"]]
+  :main hops-to.core)
---
+++ hops-to__local/test/hops_to/test/core.clj
@@ -0,0 +1,6 @@
+(ns hops-to.test.core
+  (:use [hops-to.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
