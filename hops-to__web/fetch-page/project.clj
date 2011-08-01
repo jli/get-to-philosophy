97bfa0a7325f1a8499aa23b8f42214fb7df1f93b
John Li <jli@circularly.org>
2011-07-31 23:06:40 -0400

    adding silly simple fetch-page app

---
+++ hops-to__web/fetch-page/project.clj
@@ -0,0 +1,7 @@
+(defproject fetch-page "1.0.0-SNAPSHOT"
+  :description "Fetch a page."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [ring/ring-core "0.3.11"]
+                 [ring/ring-jetty-adapter "0.3.11"]]
+  :main fetch-page.core)
---
+++ hops-to__web/fetch-page/test/fetch_page/test/core.clj
@@ -0,0 +1,6 @@
+(ns fetch-page.test.core
+  (:use [fetch-page.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
