389d8e1dd5b2984509812d7313dfbb3bd480c0ff
John Li <jli@circularly.org>
2011-09-05 22:55:21 -0400

    add gzipping

--- hops-to_web/project.clj
+++ hops-to_web/project.clj
@@ -2,6 +2,7 @@
   :description "Get to philosophy!"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojars.mikejs/ring-gzip-middleware "0.1.0-SNAPSHOT"]
                  [ring/ring-core "0.3.11"]
                  [ring/ring-devel "0.3.11"]
                  [ring/ring-jetty-adapter "0.3.11"]
--- hops-to_web/src/hops_to/core.clj
+++ hops-to_web/src/hops_to/core.clj
@@ -4,6 +4,7 @@
         [ring.middleware.file :only [wrap-file]]
         ;; [ring.middleware.reload :only [wrap-reload]]
         [ring.middleware.stacktrace :only [wrap-stacktrace]]
+        [ring.middleware.gzip :only [wrap-gzip]]
         [clojure.contrib.command-line :only [with-command-line]]
         [hops-to.parse-links :as parse-links])
   (:require [swank.swank])
@@ -38,7 +39,8 @@
      (-> #'base-app
          (wrap-file ".")
          ;; (wrap-reload '[hops-to.core hops-to.parse-links]) ; too slow
-         (wrap-stacktrace)))
+         wrap-stacktrace
+         wrap-gzip))
 
 (defn -main [& args]
   (with-command-line args
