69a53ac788d64d875c1cb87e070bcfbbbb8e9e61
John Li <jli@circularly.org>
2011-09-05 23:12:39 -0400

    bah

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
                  [ring/ring-core "0.3.11"]
                  [ring/ring-devel "0.3.11"]
                  [ring/ring-jetty-adapter "0.3.11"]
-                 [enlive "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.3.2"]]
+                 [enlive "1.0.0-SNAPSHOT"]
+                 [swank-clojure/swank-clojure "1.3.2"]]
   :main hops-to.core)
